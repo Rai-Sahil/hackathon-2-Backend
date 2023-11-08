4d46850cadf04c569f13f5fbd8e0ccf557f15b28
Sahil Rai <78111534+Rai-Sahil@users.noreply.github.com>
2023-11-08 11:16:16 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,15 @@
 const http = require('http');
-
-const corsOptions = {
-  origin: 'http://localhost:5500',
-  credentials: true,
-};
-
 const server = http.createServer((req, res) => {
-  // Check the client's origin
-  const clientOrigin = req.headers.origin;
-  console.log('Client origin: ', clientOrigin);
-
-  // Allow CORS for the specific client origin
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  // if you run the client from sth like file:///C:/nodejs/10httonlyCookie1/index.html
+  // it would mean HTML file is being served via the file:// protocol directly from your filesystem rather than over HTTP or HTTPS.
+  console.log('Request origin:', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');// Adjust the port if necessary
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-
   if (req.method === 'OPTIONS') {
-    // Preflight request automatically sent by the browser
-    res.writeHead(204); // Success with no content to return to the client
+    // Preflight request automatically sends by browser for many reasons
+    res.writeHead(204);//success with no content to return to the client
     res.end();
     return;
   }
@@ -32,9 +23,8 @@ const server = http.createServer((req, res) => {
       try {
         const { username, password } = JSON.parse(body); // Parse the JSON body
         if (username === 'admin' && password === '111') {
-
-            res.writeHead(200, {
-              'Set-Cookie': 'token=123456; HttpOnly;Max-Age=600 ',
+          res.writeHead(200, {
+            'Set-Cookie': 'token=123456; HttpOnly;Max-Age=60 ',
             'Content-Type': 'application/json',
           });
           res.end(JSON.stringify({ message: 'Logged in successfully' }));
@@ -48,25 +38,10 @@ const server = http.createServer((req, res) => {
       }
     });
     /*\ /something */
-  } else if (req.url === '/logout' && req.method === 'POST') {
-    // Check if the user is logged in by checking the cookie
-    const cookieBox = req.headers.cookie;
-    console.log('Cookie: ', cookieBox);
-    if (cookieBox && cookieBox.includes('token=123456')) {
-      res.writeHead(200, {
-        'Set-Cookie': 'token=123456; HttpOnly;Max-Age=0 ',
-        'Content-Type': 'application/json',
-      });
-      res.end(JSON.stringify({ message: 'Logged out successfully' }));
-    } else {
-      res.writeHead(401, { 'Content-Type': 'application/json' });
-      res.end(JSON.stringify({ message: 'Unauthorized: You are not logged in.' }));
-    }
   } else if (req.url === '/something' && req.method === 'GET') {
     // Check if the user is logged in by checking the cookie
-    const cookieBox = req.headers.cookie;
-    console.log('Cookie: ', cookieBox);
-    if (cookieBox && cookieBox.includes('token=123456')) {
+    const cookie = req.headers.cookie;
+    if (cookie && cookie.includes('token=123456')) {
       res.writeHead(200, { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ message: 'You are logged in, here is something.' }));
     } else {
